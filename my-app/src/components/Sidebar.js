6829f7f16dab020acffc4096fe075efea1ffc91f
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-07-01 12:48:03 +0000

    Add sidebar with links

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Sidebar() {
+    return (<div>
+                <a href="#" target="_blank">My Photos</a><br></br>
+                <a href="#" target="_blank">My Illustrations</a><br></br>
+                <a href="#" target="_blank">My Paintings</a><br></br>
+           </div>)
+}
+
+export default Sidebar
