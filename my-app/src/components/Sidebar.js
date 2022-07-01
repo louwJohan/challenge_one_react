4778256d0a5eb38984865fbca80ebcdafd433c8e
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-07-01 13:00:50 +0000

    Add link css file to sidebar

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,10 +1,11 @@
 import React from "react"
+import css from "./css/Sidebar.module.css"
 
 function Sidebar() {
-    return (<div>
-                <a href="#" target="_blank">My Photos</a><br></br>
-                <a href="#" target="_blank">My Illustrations</a><br></br>
-                <a href="#" target="_blank">My Paintings</a><br></br>
+    return (<div className={css.sidebar}>
+                <a href="#" target="_blank">My Photos</a>
+                <a href="#" target="_blank">My Illustrations</a>
+                <a href="#" target="_blank">My Paintings</a>
            </div>)
 }
 
