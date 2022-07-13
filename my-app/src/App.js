dcce3f1d7136a1695ceee0c2f14e9e8c564c8719
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-07-13 11:43:40 +0000

    Add get request and api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,8 @@ import css from './App.module.css';
 import NavBarSimple from './components/NavBarSimple';
 import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
-// import Content from './components/Content';
-import ContentHooks from './components/ContentHooks';
+import ContentAPI from './components/ContentAPI';
+
 
 
 function App() {
@@ -13,8 +13,7 @@ function App() {
       
       <NavBarForm />
       <Sidebar />
-      {/* <Content/> */}
-      <ContentHooks/>
+      <ContentAPI/>
     </div>
   );
 }
